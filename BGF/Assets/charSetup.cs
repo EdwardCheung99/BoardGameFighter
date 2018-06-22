4d7a56d98afb44b4640149833de64dd6c2434338
ProfNJ <rorygran5@gmail.com>
2018-06-22 01:50:47 -0400

    prefab fixing and list prep

---
+++ BGF/Assets/charSetup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charSetup : MonoBehaviour {
+    public int pNum, curPos, curHP, MaxHP, SK1, SK2, SK3, SK4, SK5, SK6;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BGF/Assets/tileSetup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileSetup : MonoBehaviour {
+    public int posNum, dmgMod;
+	// Use this for initialization
+	void Start () {
+    
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
