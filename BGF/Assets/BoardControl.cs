9b0cf6803df1b85d57a60617c11ecde4304d6aae
EdwardCheung99 <swsbsej@gmail.com>
2018-07-09 15:31:19 -0400

    Add Timer

--- BGF/Assets/BoardControl.cs
+++ BGF/Assets/BoardControl.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class BoardControl : MonoBehaviour {
     public GameObject tile;
-	// Use this for initialization
+    // Use this for initialization
 	void Awake () {
         //Make 5 tiles
-        for (int i = 0; i == 5; i++)
+        for (int i = 0; i == 10; i++)
         {
             GameObject b;
             //Tile Creation
             b = Instantiate(tile, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
             tileSetup pn = b.GetComponent<tileSetup>(); //Allows me to pull variables from the current tile
                pn.posNum = i;
-            if (i == 0 || i == 5)
+            if (i == 0 || i == 10)
             {
                pn.dmgMod  = 1; //Corner = +1 dmg
             }
         }
+
+
     }
 	
+    
 	// Update is called once per frame
 	void Update () {
 		
---
+++ BGF/Assets/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float thetimer = 15;
+    public Text TextTimer;
+
+
+	// Use this for initialization
+	void Start () {
+        TextTimer = GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TextTimer.text = (thetimer % 60).ToString("00");
+    }
+}
