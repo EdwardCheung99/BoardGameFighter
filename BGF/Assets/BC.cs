23a4a61870b0ae3c2bf31b79bb8147d9179777c2
ProfNJ <rorygran5@gmail.com>
2018-07-09 16:30:49 -0400

    Board Setup
    Board UI prototype complete + Tile setup formally started
    
    (its lit.)

---
+++ BGF/Assets/BC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BC : MonoBehaviour
+{
+    public Button tile;
+    public Canvas Scrin;
+
+    void Start()
+    {
+        CreateWorld();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void CreateWorld()
+    {
+
+        for (int x = 0; x < 550; x+= 55)
+        {
+                Button TPos = Instantiate(tile) as Button;
+                TPos.transform.position = new Vector3(x - 247.5f, 0, 0);
+                TPos.transform.SetParent(Scrin.transform, false);
+        }
+    }
+
+}
