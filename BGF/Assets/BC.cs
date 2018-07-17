1dc7e9624bdb3a6663ec4af044b7134c50aa9753
EdwardCheung99 <swsbsej@gmail.com>
2018-07-17 17:33:56 -0400

    Added tileSetup script and Neutral Phase Text

--- BGF/Assets/BC.cs
+++ BGF/Assets/BC.cs
@@ -7,10 +7,12 @@ public class BC : MonoBehaviour
 {
     public Button tile;
     public Canvas Scrin;
+    
 
     void Start()
     {
-        CreateWorld();
+        BeginNeutral();
+
     }
 
     void Update()
@@ -21,13 +23,39 @@ public class BC : MonoBehaviour
     void CreateWorld()
     {
 
-        for (int x = 0; x < 550; x+= 55)
+        for (int x = 0; x < 550; x += 55)
         {
-                Button TPos = Instantiate(tile) as Button;
-                TPos.transform.position = new Vector3(x - 247.5f, 0, 0);
-                TPos.transform.SetParent(Scrin.transform, false);
-                TPos.GetComponentInChildren<Text>().text = "";
+            Button TPos = Instantiate(tile) as Button;
+            TPos.transform.position = new Vector3(x - 247.5f, 0, 0);
+            TPos.transform.SetParent(Scrin.transform, false);
+            TPos.GetComponent<tileSetup>().posNum = (x / 55) + 1;
+            if (TPos.GetComponent<tileSetup>().posNum == 1 || TPos.GetComponent<tileSetup>().posNum == 10)
+            {
+                TPos.GetComponent<tileSetup>().dmgMod = 1;
+            }
+            TPos.GetComponentInChildren<Text>().text = "";
         }
+    }
+
+    void BeginNeutral()
+    {
+        //Begin by making GO for text to tell players what to do
+        Text StartText_Text;
+        RectTransform StartText_RectTrans;
+
+        GameObject newGO = new GameObject("NeutralText");
+        newGO.transform.SetParent(Scrin.transform);
+
+        Text StartText = newGO.AddComponent<Text>();
+        StartText.text = "Neutral Phase Begin!";
+
+        StartText_Text = StartText.GetComponent<Text>();
+        StartText_RectTrans = StartText.GetComponent<RectTransform>();
+
+        StartText_Text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        StartText_Text.fontSize = 30;
+        StartText_RectTrans.sizeDelta = new Vector2(StartText_Text.fontSize * 10, 100);
+        StartText.rectTransform.localPosition = new Vector3(0f, 0f);
     }
 
 }
--- BGF/Assets/BoardControl.cs
+++ BGF/Assets/BoardControl.cs
@@ -29,7 +29,5 @@ public class BoardControl : MonoBehaviour {
 		
 	}
 
-    void BeginNeutralPhase (){
-        
-    }
+
 }
