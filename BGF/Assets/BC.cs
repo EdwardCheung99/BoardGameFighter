158e380cfe76079d96d07c18574079f692245e30
EdwardCheung99 <swsbsej@gmail.com>
2018-08-07 15:38:37 -0400

    Added guessies

--- BGF/Assets/BC.cs
+++ BGF/Assets/BC.cs
@@ -7,23 +7,29 @@ public class BC : MonoBehaviour
 {
     public Button tile;
     public Canvas Scrin;
-    public float TimeSt;
     public float TimeLen = 3f;
-    public string guess = "";
+    public string guess1 = "";
+    public string guess2 = "";
+    public Text NeutralText;
+    public string Neutral2 = "Please make your guess.";
+    public bool GuessTime = false;
+    public bool GuessTime1 = true;
+    public bool GuessTime2 = true;
 
     void Start()
     {
+        
         BeginNeutral();
-
+        StartCoroutine(BeginToGuess());
     }
 
-    void Update()
+    IEnumerator BeginToGuess()
     {
-        if (Time.time> TimeSt + TimeLen)
-        {
-            //change text
-        }
+        yield return new WaitForSeconds(3);
+        NeutralText.text = Neutral2;
+        GuessTime = true;
     }
+
 
     void CreateWorld()
     {
@@ -44,11 +50,22 @@ public class BC : MonoBehaviour
 
     void OnGUI()
     {
-        if (Event.current.Equals(Event.KeyboardEvent("k")))
+        if (GuessTime == true)
         {
-            guess = "high";
-            Debug.Log("Guess done");
-            //After this is inputted, it switches to the next scene w/ guess variables
+            if ((Event.current.Equals(Event.KeyboardEvent("j"))) && GuessTime1)
+            {
+                guess1 = "high";
+                Debug.Log("P1 Guess done it is " + guess1);
+                GuessTime1 = false;
+                //After this is inputted, it switches to the next scene w/ guess variables
+            }
+
+            if ((Event.current.Equals(Event.KeyboardEvent("a"))) && GuessTime2) 
+            {
+                guess2 = "high";
+                Debug.Log("P2 Guess done, it is " + guess2);
+                GuessTime2 = false;
+            }
         }
 
     }
@@ -56,26 +73,25 @@ public class BC : MonoBehaviour
     void BeginNeutral()
     {
         //Begin by making text to signal beginning of neutral
-        Text StartText_Text;
-        RectTransform StartText_RectTrans;
 
-        GameObject newGO = new GameObject("NeutralText"); //make GO for text
-        newGO.transform.SetParent(Scrin.transform);
-        Text StartText = newGO.AddComponent<Text>();
-        StartText.text = "Neutral Phase Begin!";
+        //Text StartText_Text;
+        //RectTransform StartText_RectTrans;
 
-        StartText_Text = StartText.GetComponent<Text>();
-        StartText_RectTrans = StartText.GetComponent<RectTransform>();
-        StartText_Text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-        StartText_Text.fontSize = 30;
-        StartText_RectTrans.sizeDelta = new Vector2(StartText_Text.fontSize * 10, 100);
-        StartText.rectTransform.localPosition = new Vector3(0f, 0f);
+        //GameObject newGO = new GameObject("NeutralText"); //make GO for text
+        //newGO.transform.SetParent(Scrin.transform);
+        //Text StartText = newGO.AddComponent<Text>();
+        //StartText.text = "Neutral Phase Begin!";
 
-        //Guess phase begins, players select how they choose to block
+        //StartText_Text = StartText.GetComponent<Text>();
+        //StartText_RectTrans = StartText.GetComponent<RectTransform>();
+        //StartText_Text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        //StartText_Text.fontSize = 30;
+        //StartText_RectTrans.sizeDelta = new Vector2(StartText_Text.fontSize * 10, 100);
+        //StartText.rectTransform.localPosition = new Vector3(0f, 0f);
 
-        
+        //Guess phase begins, players select how they choose to block        
 
-        if (guess != "")
+        if ((guess1 != "") && (guess2 != ""))
         {
             //go to next scene
         }
